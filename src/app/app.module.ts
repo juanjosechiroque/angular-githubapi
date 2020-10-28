dbfa2f0c9edd8d56b9a18c67800b7626f9268adf
juanjosechiroque <juanjosechiroque@gmail.com>
2020-10-27 23:41:00 -0400

    add list of commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ListCommits } from "./components/list-commits/list-commits.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [    
     ListCommits
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ListCommits]
--- src/app/components/list-commits/list-commits.component.ts
+++ src/app/components/list-commits/list-commits.component.ts
@@ -1,14 +1,32 @@
 import { Component } from "@angular/core";
+import { CommitService } from "../../services/commit.service";
 
 @Component({
   selector: "list-commits",
-  template: `<h1>List Commits<h1>`  
+  templateUrl: "./list-commits.component.html"  
 })
 
 export class ListCommits {
   
-  constructor(){
-    console.log("load");
+  public listCommits: Array<any> = [];
+  
+  constructor(
+    private commitService: CommitService
+  ){
+      
+    this.commitService.getCommits().subscribe((r: any) => {      
+      this.listCommits = r;
+      console.log(this.listCommits);
+      console.log(this.listCommits[0]);
+    });
+      
   }
   
 }
+
+
+
+
+
+
+
